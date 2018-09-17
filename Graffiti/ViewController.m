d750b520ce5e90ec59f9ca63229d24b5ba8b0fb5
dalianmengzi <dalianmengzi@users.noreply.github.com>
2018-09-17 14:00:52 +0800

    add

--- Graffiti/ViewController.m
+++ Graffiti/ViewController.m
@@ -8,22 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController (){
+    NSArray* list;
+}
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    list = @[@"涂鸦"];
+    [self setTableView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)setTableView{
+       [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"MyCell"];
+       self.tableView.tableFooterView = [[UIView alloc] init];
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
 
+    return list.count;
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyCell" forIndexPath:indexPath];
+    NSString *name = list[indexPath.row];
+    cell.textLabel.text = name;
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    NSLog(@"%@", list[indexPath.row]);
+}
 @end
